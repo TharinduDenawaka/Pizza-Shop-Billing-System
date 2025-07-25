c16e7386c03ff4f944123c4690e39e525c52c0ca
TharinduDenawaka <tharindudasun1997@gmail.com>
2025-07-25 13:09:43 +0530

    Add Hosting DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,45 +1,18 @@
 // Local postgreSQL db connection 
-const { Pool } = require('pg');
-require('dotenv').config();
-
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
-
-pool.query('SELECT NOW()', (err) => {
-  if (err) {
-    console.error(' Connection error:', err.message);
-  } else {
-    console.log('Successfully connected to PostgreSQL');
-  }
-});
-
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
-
-
-
-//Connect hosted DB from neone
 // const { Pool } = require('pg');
 // require('dotenv').config();
 
-
 // const pool = new Pool({
-//   connectionString: process.env.DB_CONNECTION_STRING,
-//   ssl: {
-//     rejectUnauthorized: false 
-//   }
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
 // });
-
 
 // pool.query('SELECT NOW()', (err) => {
 //   if (err) {
-//     console.error('Connection error:', err.message);
+//     console.error(' Connection error:', err.message);
 //   } else {
 //     console.log('Successfully connected to PostgreSQL');
 //   }
@@ -48,3 +21,30 @@ module.exports = {
 // module.exports = {
 //   query: (text, params) => pool.query(text, params),
 // };
+
+
+
+//Connect hosted DB from neone
+const { Pool } = require('pg');
+require('dotenv').config();
+
+
+const pool = new Pool({
+  connectionString: process.env.DB_CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false 
+  }
+});
+
+
+pool.query('SELECT NOW()', (err) => {
+  if (err) {
+    console.error('Connection error:', err.message);
+  } else {
+    console.log('Successfully connected to PostgreSQL');
+  }
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
